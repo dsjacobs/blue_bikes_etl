aa1a7f62742d2517a2770a4c07b56dd11d68c7bb
Danielle Jacobs <47550694+dsjacobs@users.noreply.github.com>
2025-10-17 14:37:04 -0400

    separate stations from rides

--- etl.py
+++ etl.py
@@ -1,4 +1,5 @@
-from etl_helpers import lnd
+from etl_helpers import lnd, run_sql
 
 src_url = "https://s3.amazonaws.com/hubway-data/202504-bluebikes-tripdata.zip"
 lnd(src_url)
+run_sql("schema.sql")
---
+++ schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE stations AS (
+    SELECT DISTINCT start_station_id AS id,
+        start_station_name AS station_name 
+    FROM lnd
+    UNION SELECT end_station_id,
+        end_station_name
+    FROM lnd
+);
+
+CREATE TABLE rides AS (
+SELECT 
+    ride_id,
+    rideable_type,
+    started_at,
+    ended_at,
+    start_station_id,
+    end_station_id,
+    member_casual
+);
