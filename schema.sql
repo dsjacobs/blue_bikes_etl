86c40096a8d233f19787c0ae73453ed05e7c1412
Danielle Jacobs <47550694+dsjacobs@users.noreply.github.com>
2025-10-17 15:03:05 -0400

    if not exists and rename timestamps to ts

--- schema.sql
+++ schema.sql
@@ -1,20 +1,22 @@
-CREATE TABLE stations AS 
+CREATE TABLE IF NOT EXISTS stations AS 
     SELECT DISTINCT start_station_id AS id,
-        start_station_name AS station_name 
+        start_station_name AS station_name,
+        start_lat AS lat,
+        start_long AS long 
     FROM lnd
     UNION SELECT end_station_id,
-        end_station_name
-    FROM lnd
-;
+        end_station_name,
+        end_lat AS lat,
+        end_long AS long
+    FROM lnd;
 
-CREATE TABLE rides AS 
+CREATE TABLE IF NOT EXISTS rides AS 
 SELECT 
     ride_id,
     rideable_type,
-    started_at,
-    ended_at,
+    started_at AS start_ts,
+    ended_at AS end_ts,
     start_station_id,
     end_station_id,
     member_casual
-FROM lnd
-;
+FROM lnd;
