8b2ec6c3939f872b9209fa8ae40b43eafc54f8df
Danielle Jacobs <47550694+dsjacobs@users.noreply.github.com>
2025-10-18 21:09:44 -0400

    bikes left and arrived from same station

---
+++ busy_stations.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS station_day_arrivals_departures AS
+WITH leaving AS (
+	SELECT start_station_id AS station_id,
+    	STRFTIME('%m', started_at) AS ride_month,
+		STRFTIME('%d', started_at) AS ride_date,
+		count(*) AS ride_count
+	FROM rides
+	GROUP BY 1,2,3
+),
+arriving AS (
+	SELECT end_station_id AS station_id,
+    	STRFTIME('%m', ended_at) AS ride_month,
+		STRFTIME('%d', ended_at) AS ride_date,
+		count(*) AS ride_count
+	FROM rides
+	GROUP BY 1,2,3
+)
+SELECT s.id station_id,
+	s.station_name,
+    leaving.ride_month || '-' || leaving.ride_date AS ride_date,
+	leaving.ride_count bikes_left,
+	arriving.ride_count bikes_returned
+FROM stations s
+LEFT JOIN leaving ON s.id = leaving.station_id
+LEFT JOIN arriving ON s.id = arriving.station_id 
+WHERE arriving.ride_month = leaving.ride_month
+	AND arriving.ride_date = leaving.ride_date
+;
